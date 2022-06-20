172878915a7dd2d7af6eccc01f217f514fe2c69c
abetx10 <abetx10@gmail.com>
2022-06-20 07:41:46 +0100

    ViewHolder_ok

---
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shoppinglist.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: ShopListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupRecycleView()
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this) {
+            adapter.shopList = it
+
+        }
+
+    }
+
+    private fun setupRecycleView() {
+        val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+        adapter = ShopListAdapter()
+        rvShopList.adapter = adapter
+    }
+
+
+}
